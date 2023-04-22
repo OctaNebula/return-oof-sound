0bf6d1ed060dfdeb41aefd1923d53ee4bd3d44d9
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-04-22 21:43:36 +0200

    fixed winshell being coded inproperly

--- installer.py
+++ installer.py
@@ -1,6 +1,6 @@
 import os
 import urllib.request as urllib2
-import winshell as ws
+import winshell
 import tkinter as tk
 
 #creates directory in c:\Program Files (x86)\ReturnOof
@@ -18,7 +18,7 @@ urllib2.urlretrieve("https://github.com/OctaNebula/return-oof-sound/raw/main/ouc
 
 #creates a shortcut to the main.py file in the startup folder
 
-ws.CreateShortcut(Path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ReturnOof.lnk", Target="C:\Program Files (x86)\ReturnOof\main.pyw", Icon="C:\Program Files (x86)\ReturnOof\ouch.ogg", Description="Return Oof Sound")
+winshell.CreateShortcut(Path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ReturnOof.lnk", Target="C:\Program Files (x86)\ReturnOof\main.pyw", Icon="C:\Program Files (x86)\ReturnOof\ouch.ogg", Description="Return Oof Sound")
 
 #creates a popup window to tell the user that the program has been installed
 
