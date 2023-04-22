a47763cdebc96fde21f4143b32bc408f83449db0
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-04-22 21:23:49 +0200

    Should now notify the user when it finishes the installation

---
+++ installer.py
@@ -0,0 +1,31 @@
+import os
+import urllib.request as urllib2
+import winshell as ws
+import tkinter as tk
+
+#creates directory in c:\Program Files (x86)\ReturnOof
+
+path = "C:\Program Files (x86)\ReturnOof"
+os.mkdir(path)
+
+#downloads main.py to c:\Program Files (x86)\ReturnOof from github
+
+urllib2.urlretrieve("https://raw.githubusercontent.com/OctaNebula/return-oof-sound/main/main.py", "C:\Program Files (x86)\ReturnOof\main.py")
+
+#literally the same but for the sound file
+
+urllib2.urlretrieve("https://github.com/OctaNebula/return-oof-sound/raw/main/ouch.ogg", "C:\Program Files (x86)\ReturnOof\ouch.ogg")
+
+#creates a shortcut to the main.py file in the startup folder
+
+ws.CreateShortcut(Path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ReturnOof.lnk", Target="C:\Program Files (x86)\ReturnOof\main.py", Icon="C:\Program Files (x86)\ReturnOof\ouch.ogg", Description="Return Oof Sound")
+
+#creates a popup window to tell the user that the program has been installed
+
+root = tk.Tk()
+root.title("Return Oof Sound")
+root.geometry("300x100")
+root.resizable(False, False)
+label = tk.Label(root, text="Return Oof Sound has been installed!")
+label.pack()
+root.mainloop()
