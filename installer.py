95b0f32c73b3893a540871166705c7f11806cf12
OctaNebula <68815420+OctaNebula@users.noreply.github.com>
2023-04-22 22:40:02 +0200

    reverted commits

--- installer.py
+++ installer.py
@@ -7,27 +7,27 @@ import tkinter as tk
 
 os.system("pip install winshell")
 
-#creates directory in C:\Users\%userprofile%\Documents\ReturnOof
+#creates directory in c:\Program Files (x86)\ReturnOof
 
-os.makedirs(r"C:\Users\%userprofile%\Documents\ReturnOof\ReturnOof")
+os.mkdir("C:\Program Files (x86)\ReturnOof")
 
-#downloads main.py to C:\Users\%username%\Documents\ReturnOof from github
+#downloads main.py to c:\Program Files (x86)\ReturnOof from github
 
-urllib2.urlretrieve("https://raw.githubusercontent.com/OctaNebula/return-oof-sound/main/main.pyw", r"C:\Users\%userprofile%\Documents\ReturnOof\main.pyw")
+urllib2.urlretrieve("https://raw.githubusercontent.com/OctaNebula/return-oof-sound/main/main.pyw", "C:\Program Files (x86)\ReturnOof\main.pyw")
 
 #literally the same but for the sound file
 
-urllib2.urlretrieve("https://github.com/OctaNebula/return-oof-sound/raw/main/ouch.ogg", r"C:\Users\%userprofile%\Documents\ReturnOof\ouch.ogg")
+urllib2.urlretrieve("https://github.com/OctaNebula/return-oof-sound/raw/main/ouch.ogg", "C:\Program Files (x86)\ReturnOof\ouch.ogg")
 
 #creates a shortcut to the main.py file in the startup folder
 
-winshell.CreateShortcut(Path=r"C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ReturnOof.lnk", Target=r"C:\Users\%userprofile%\Documents\ReturnOof\main.pyw")
+winshell.CreateShortcut(Path="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\ReturnOof.lnk", Target="C:\Program Files (x86)\ReturnOof\main.pyw")
 
 #creates a popup window to tell the user that the program has been installed
 
 #runs the main.py file
 
-os.system(r"C:\Users\%userprofile%\Documents\ReturnOof\main.pyw")
+os.system("C:\Program Files (x86)\ReturnOof\main.pyw")
 
 root = tk.Tk()
 root.title("Return Oof Sound")
